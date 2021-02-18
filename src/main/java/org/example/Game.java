116834c9a7e0d89a1432cb0eb68a88986f39d479
Barry Smith, Deepak Sisodia and Gary Hackathorn <pair+Barry.Smith+deepak.sisodia+gary.hackathorn@CVSHealth.com>
2021-02-17 22:23:24 -0600

    solution commit

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -1,6 +1,40 @@
 package org.example;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
+    List<Integer> rolls = new ArrayList<>();
+
+    public void rollMulti(int... rolls) {
+        for (int pins : rolls) {
+            this.roll(pins);
+        }
+    }
+
+    public void roll(int pins) {
+        rolls.add(pins);
+    }
+
+    public int score() {
+        int total = 0;
+        int rollNumber = 0;
+        int frames = 0;
+        while (frames < 10) {
+            if (rolls.get(rollNumber) == 10) {
+                total = total + rolls.get(rollNumber) + rolls.get(rollNumber+1) + rolls.get(rollNumber+2);
+                rollNumber++;
+            } else {
+                total = total + rolls.get(rollNumber) + rolls.get(rollNumber + 1);
+                if (rolls.get(rollNumber) + rolls.get(rollNumber + 1) == 10) {
+                    total = total + rolls.get(rollNumber + 2);
+                }
+                rollNumber += 2;
+            }
+            frames++;
+        }
+        return total;
+    }
 
 }
--- src/test/java/org/example/GameTest.java
+++ src/test/java/org/example/GameTest.java
@@ -1,7 +1,58 @@
 package org.example;
 
-class GameTest{
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GameTest{
+    private Game game;
+
+    @BeforeEach
+    void setUp() {
+        game = new Game();
+    }
+
+    private void rollMany(int pins, int times) {
+        for (int i = 0; i < times; i++) {
+            game.roll(pins);
+        }
+    }
+
+    @Test
+    public void allGutterBalls()  {
+        rollMany(0, 20);
+        assertEquals(0, game.score());
+    }
+
+    @Test
+    public void allOnes() {
+        rollMany(1, 20);
+        assertEquals(20, game.score());
+    }
+
+    @Test
+    @DisplayName("A spare fulled by 3 pull all 0s = 16")
+    public void aSpare() {
+        game.rollMulti(1,9,3);
+        rollMany(0, 17);
+        assertEquals(16, game.score());
+    }
+
+    @Test
+    @DisplayName("10 folled by 3 and 4 plus all misses = 244")
+    public void testStrike() {
+        game.rollMulti(10, 3,4);
+        rollMany(0, 16);
+        assertEquals(24, game.score());
+    }
+    @Test
+    @DisplayName("A perfect score 12 strikes = 300")
+    public void perfectGame() {
+        rollMany(10, 12);
+        assertEquals(300, game.score());
+    }
 }
 //Uncle Bob breaks this kata down into the following five tests:
 //
